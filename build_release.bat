89ac7f61e4afd9be562964a21006742a23663bf1
Kody Brown <thewizard@wasatchwizard.com>
2025-12-30 10:30:53 -0700

    trying to get the built binaries part of the release

--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,8 @@
 @echo off
 cd "%~dp0"
 cargo build --release
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+copy /y "C:\tmp\_rust\code_scroller\target\release\code_scroller.exe" "."
+copy /y "C:\tmp\_rust\code_scroller\target\release\code_scroller" "."
